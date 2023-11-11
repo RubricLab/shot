95eb8e4975ae50bcce2c3a4bf1be64beb39d6ff9
tedspare <ted.spare@gmail.com>
2023-11-11 12:18:02 -0500

    Add upload func to API

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,30 @@
 import puppeteer from "puppeteer-core";
+import { put } from "@vercel/blob";
+
+console.log("🚀 Starting up on port " + process.env.PORT);
+
+const headers = {
+  common: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Methods": "GET, POST",
+  },
+};
 
 Bun.serve({
   hostname: "::",
   port: process.env.PORT || 3001,
   fetch: async (request: Request) => {
-    console.log("Starting up on " + process.env.PORT);
-
     const { searchParams } = new URL(request.url);
 
     let url = searchParams.get("url");
+    let upload = Boolean(searchParams.get("upload"));
 
-    if (!url) return new Response("Do ?url=https://example.com");
+    if (!url) return new Response("Add ?url=example.com");
+
+    console.log(
+      `📸 Taking screenshot of ${url}. ${upload ? "Uploading." : ""}`
+    );
 
     url = url.startsWith("https://") ? url : `https://${url}`;
 
@@ -22,22 +36,36 @@ Bun.serve({
 
     // Wait until network activity settles to 2 req/s
     await page.goto(url, { waitUntil: "networkidle2" });
-
-    await page.setViewport({
-      width: 1920,
-      height: 1080,
-    });
+    await page.setViewport({ width: 1920, height: 1080 });
 
     const screenshot = await page.screenshot();
 
-    const res = new Response(screenshot);
+    let res: any;
 
-    res.headers.set("Content-Type", "image/png");
+    if (!upload) {
+      res = new Response(screenshot, {
+        headers: {
+          "Content-Type": "image/png",
+          ...headers.common,
+        },
+      });
+    } else {
+      const uid = Math.random().toString(36).substring(2, 15);
+      const strippedUrl = url.replace(/(^\w+:|^)\/\//, "");
+      const fileName = `${strippedUrl}-${uid}.png`;
+      const blob = new Blob([screenshot]);
 
-    // Set generous CORS headers
-    res.headers.set("Access-Control-Allow-Origin", "*");
-    res.headers.set("Access-Control-Allow-Headers", "*");
-    res.headers.set("Access-Control-Allow-Methods", "GET, POST");
+      const upload = await put(fileName, blob, { access: "public" });
+
+      res = new Response(upload.url, {
+        headers: {
+          "Content-Type": "text/plain",
+          ...headers.common,
+        },
+      });
+    }
+
+    await browser.close();
 
     return res;
   },
