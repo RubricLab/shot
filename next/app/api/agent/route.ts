e30dcae2abef5fff8b6055d53336e034fab5d29c
tedspare <ted.spare@gmail.com>
2023-11-10 20:43:22 -0500

    Remove unused files

--- next/app/api/agent/route.ts
+++
@@ -1,9 +0,0 @@
-import {basicAgent} from '~/agents/basic'
-
-export async function POST(req) {
-	const { input } = await req.json()
-	
-	const stream = basicAgent({input})
-	
-	return new Response(await stream)
-}
